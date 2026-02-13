2b731554f24e14ccd8d1e5cf4bc31532a0d79579
Jair Garces <Garc8185@stthomas.edu>
2026-02-12 18:13:25 -0600

    timetotest

--- pointers.cpp
+++ pointers.cpp
@@ -20,9 +20,50 @@ int main(int argc, char **argv)
     double average;
 
     // Sequence of user input -> store in fields of `student`
+    student.id = promptInt("Please enter the student's id number: ", 0, 999999999);
+
+    //get first name
+    std::cout << "Please enter the student's first name: ";
+    std::cin.getline(student.f_name,128);
+
+    //get last name
+    std::cout << "Please enter the student's last name: ";
+    std::cin.getline(student.l_name,128);
+
+    //number of assignments
+    student.n_assignments = promptInt("Please enter how many assignments were graded: ", 1, 134217727);
+
+    //allocate memory dynamically for grades
+    student.grades = new double[student.n_assignments];
+
+    std::cout << std::endl;
+
+    //get grades
+    for(int i=0; i<student.n_assignments;i++){
+        student.grades[i] = promptDouble("Please enter grade for assignment" + std::to_string(i) + ": ", 0.0, 1000.0);
+    }
+
+    std::cout << std::endl;
+
+
+
+
+
+
+
+    
 
     // Call `CalculateStudentAverage(???, ???)`
+    calculateStudentAverage(&student,&average);
+    double round = (int)(average*10+0.5)/10.0;
+
     // Output `average`
+    std::cout << "Student: " << student.f_name << " " << student.l_name 
+    << " [" << student.id << "]" << std::endl;
+
+    std::cout << "  Average grade: " << round << std::endl;
+
+    delete[] student.grades;
 
     return 0;
 }
@@ -34,7 +75,29 @@ int main(int argc, char **argv)
 */
 int promptInt(std::string message, int min, int max)
 {
+    std::string tempstr;
     // Code to prompt user for an int
+    while(true){
+        std::cout << message;
+        std::getline(std::cin,tempstr);
+
+        try{
+            size_t numconv;
+            int value = std::stoi(tempstr, &numconv);//convert to int, track number of items converted
+            
+            if(numconv != tempstr.length())
+            throw std::invalid_argument("non number input");
+
+            if(value < min || value > max)
+            throw std::out_of_range("out of range");
+
+            return value;
+        }
+        catch(...){
+            std::cout << "Sorry, I cannot understand your answer" << std::endl;
+        }
+    }
+
 }
 
 /*
@@ -44,7 +107,30 @@ int promptInt(std::string message, int min, int max)
 */
 double promptDouble(std::string message, double min, double max)
 {
+    std::string buildstr;
     // Code to prompt user for a double
+
+    while(true){
+        std::cout << message;
+        std::getline(std::cin, buildstr);
+
+        try{
+            size_t nconvert;
+            double value = std::stod(buildstr, &nconvert);//convert the string to double, also grabs the number of items converted
+            
+            if(nconvert!=buildstr.length())
+            throw std::invalid_argument("non number was input");
+
+            if(value < min || value > max)
+            throw std::out_of_range("out of range");
+
+            return value;
+        }
+        catch(...){
+            std::cout << "Sorry, I cannot understand your answer" << std::endl;
+        }
+
+    }
 }
 
 /*
@@ -53,5 +139,13 @@ double promptDouble(std::string message, double min, double max)
 */
 void calculateStudentAverage(void *object, double *avg)
 {
+    Student *student = (Student*)object; //cast to student struct pointer
+
     // Code to calculate and store average grade
+    double sum = 0.0;
+
+    for(int i=0;i<student->n_assignments;i++){
+        sum += student->grades[i];
+    }
+    *avg = sum/student->n_assignments;
 }
